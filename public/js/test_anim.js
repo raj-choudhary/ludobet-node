d17c477e1a7728803ea2a5fc2cc49d63b2fb6a6f
Rajendra Choudhary <rc059602@gmail.com>
2026-08-24 21:43:14 +0530

    chore(clean): remove all scratch script files and maintain pure MVC architecture

--- public/js/test_anim.js
+++
@@ -1,9 +0,0 @@
-const sharp = require('sharp');
-const path = require('path');
-const fs = require('fs');
-
-async function testSharpAnimation() {
-  console.log('Sharp formats:', sharp.format);
-}
-
-testSharpAnimation().catch(console.error);
